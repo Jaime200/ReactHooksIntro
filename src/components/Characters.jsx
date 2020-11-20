9f8e872928b487aa0f799af9090a8a2492f25077
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-11-20 15:58:02 -0600

    Inicio de useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import '../style/Characters.css'
 import Character from './Character'
+
+const initialState = {
+    favorites : []
+}
+
+const favoriteReducer = (state, action) =>{
+    switch(action.type){
+        case  'ADD_TO_FAVORITE': 
+            return {
+                ...state ,
+                favorites : [...state.favorites, action.payload]
+            }
+        default : return state;
+        
+    }
+}
+
 const Characters = ()=>{
     
     const [characters, setCharacters ] = useState([])
-
+    const [ favorites, dispatch ] = useReducer(favoriteReducer(initialState))
     const fetchData = async () => {
         const response = await fetch("https://rickandmortyapi.com/api/character");
         const data = await response.json();
